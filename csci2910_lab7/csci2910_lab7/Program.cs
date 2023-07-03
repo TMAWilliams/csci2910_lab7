9739cd48a6ef8a08334436632492da5819572d1b
TMAWilliams <tmawilliams73@gmail.com>
2023-07-03 11:53:12 -0400

    Set up project

---
+++ csci2910_lab7/csci2910_lab7/Program.cs
@@ -0,0 +1,20 @@
+﻿/**       
+ *--------------------------------------------------------------------
+ * 	   File name: Program.cs
+ * 	Project name: csci2910_lab7
+ *--------------------------------------------------------------------
+ * Author’s name and email:	 Tessa Williams williamstm5@etsu.edu				
+ *          Course-Section: CSCI-2910-970
+ *           Creation Date:	 07/03/2023		
+ * -------------------------------------------------------------------
+ */
+namespace csci2910_lab7
+{
+    public class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
